ddaa8df9cce629f5767879938fd526710fa73d55
spaceofmiah <geek_200@outlook.com>
2019-10-01 00:38:06 +0100

    Retrieve concept content and add to DOM

--- dist/script/index.js
+++ dist/script/index.js
@@ -1,6 +1,44 @@
 const concept_links_id = [
     "functions", "partials", "nesting", "variable"];
 
+const articles = {
+    variable : {
+        code:`
+            $primary-color: #fccd33;<br />
+            $regular-size: 18px;<br /><br />
+            
+            p {<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;color: $primary-color;<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;font-size: $regular-size;<br />
+            }<br /><br />
+        `,
+        writeup: `
+            variables are like container that holds a value for later usage.
+            sass' variables are compatible with all browsers, as it passes
+            the initial value assigned/stored in it during compilation.
+            <br />
+            The displayed code sample show a quick and non exhaustive way a 
+            variable is used in sass to set color and font-size of a paragraph
+            element.
+        `
+    },
+
+    functions: {
+        code : ``,
+        writeup: ``
+    },
+
+    partials: {
+        code: ``,
+        writeup: ``
+    },
+
+    nesting: {
+        code: ``,
+        writeup: ``
+    }
+}
+
 concept_links_id.forEach(link => {
     // set event listener to all links
     let linkDomElement = document.getElementById(link);
@@ -12,6 +50,7 @@ concept_links_id.forEach(link => {
 const handler = (src) => {
     handleActiveLinkChange(src);
     handleArticleHeaderChange(src);
+    handleArticleContentChange(src);
 }
 
 const handleArticleHeaderChange = (src) => {
@@ -29,8 +68,15 @@ const handleActiveLinkChange = (src) => {
     // use it as an id in retrieving the concept_info link which will
     // enable us to remove the active class from it's class list.
     let article_heading = document.querySelector(".article__heading").textContent.toLowerCase();
-    let article_link = document.getElementById(article_heading);
-    article_link.classList.remove('active');
-
+    let link = concept_links_id.filter(link => link === article_heading);
+    document.getElementById(link).classList.remove('active');
+    
     src.classList.add('active');
+}
+
+
+const handleArticleContentChange = (src) => {
+    let article = articles[src.id]
+    document.querySelector('code').innerHTML = article.code;
+    document.querySelector(".article__content").innerHTML = article.writeup;
 }
