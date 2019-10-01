ce97b212d7176def85392d97569c15dc5ca24c47
spaceofmiah <geek_200@outlook.com>
2019-10-01 01:55:55 +0100

    Create content 4 other concept links article

--- dist/script/index.js
+++ dist/script/index.js
@@ -8,8 +8,8 @@ const articles = {
             $regular-size: 18px;<br /><br />
             
             p {<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;color: $primary-color;<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;font-size: $regular-size;<br />
+                &ensp;&ensp;color: $primary-color;<br />
+                &ensp;&ensp;font-size: $regular-size;<br />
             }<br /><br />
         `,
         writeup: `
@@ -23,19 +23,82 @@ const articles = {
         `
     },
 
+
     functions: {
-        code : ``,
-        writeup: ``
+        code : `
+             @function get_value($value) { <br />
+                 &ensp;&ensp;@return map-get($map_object, $value); <br/>
+             } <br/> <br />
+        `,
+        writeup: `
+        i'm glad to inform you that functions in sass behaves like function
+        in programming languages, the only difference is in the syntax.
+        <br/>
+        The displayed code helps to retrieve a distinct value from a map 
+        object (you can read more on map) using the argument ($value) 
+        passed in the function.
+        `
     },
 
     partials: {
-        code: ``,
-        writeup: ``
+        code: `
+        // (variable module) named <br />
+        // _variables.scss file <br/>
+
+        $primary-color: #fcc3ee; <br/>
+        $regular-font: 18px;<br/>
+
+        <br/>
+        // main.scss<br/>
+        @import "./path/to/varibles";<br/>
+
+        body {<br/>
+
+            &ensp;&ensp;background: $primary-color; <br/>
+            &ensp;&ensp;font-size: $regular-font; </br>
+            
+        }<br/>
+        `,
+        writeup: `
+        Partials in sass are identical to modules, which helps to structure
+        code base and aids keeping of identical code in thier distinct files.
+        <br />
+        Partials are the sole drive for separation of concern in sass, which
+        allows developer to focus on a specific development phase in a project.
+        <br />
+        It should be noted that partils filed should be named with a leading
+        underscore. e.g a partial file to be called 'variable', will be saved
+        with '_variable' and also, when importing partials, the leading underscore
+        and the extension are not needed.
+        `
     },
 
     nesting: {
-        code: ``,
-        writeup: ``
+        code: `
+        .nav { <br /> &ensp;&ensp;
+            background: #fcc3dd; <br /> &ensp;&ensp;
+            padding: 18px; <br /> 
+            
+            <br /> &ensp;&ensp;
+            #{&}__heading { <br />
+                &ensp;&ensp;&ensp;&ensp;font-weight: 400; <br />
+                &ensp;&ensp;&ensp;&ensp;color: #33ffcc; <br />&ensp;&ensp;
+            } <br />
+            
+            <br />&ensp;&ensp;
+            #{&}__motto {    <br /> 
+                &ensp;&ensp;&ensp;&ensp;font-size: 16px; <br />&ensp;&ensp;
+            } <br />
+        } <br />
+        `,
+        writeup: `
+            The beauty of writing HTML is in it's nesting ability, which
+            help to create a parent and child relationship in markups. 
+            This same functionality is a core feature in sass and it 
+            doesn't just allows for parent -- child relationship, but
+            when used properly, can enable the creation of components 
+            on HTML. 
+        `
     }
 }
 
